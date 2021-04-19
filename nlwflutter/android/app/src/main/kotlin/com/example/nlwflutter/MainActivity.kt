8d01eb4875a352a8f208808c3c189cca6cdd213b
Otniel Silva <otnielsilvag4@gmail.com>
2021-04-19 16:16:54 -0300

    criação do projeto

---
+++ nlwflutter/android/app/src/main/kotlin/com/example/nlwflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nlwflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ nlwflutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
+
